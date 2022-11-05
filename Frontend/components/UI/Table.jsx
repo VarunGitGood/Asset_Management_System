a9f6c10fa3457b3216eea57de9f27347e21ccc61
sammybh6 <samyakbhargava9@gmail.com>
2022-11-05 23:13:23 +0530

    p4

--- Frontend/components/UI/Table.jsx
+++ Frontend/components/UI/Table.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import AssetCard from './AssetCard'
 import TableStyles from '../style/Table.module.css'
+import { useParams, Link } from "react-router-dom";
 import { FetchData } from '../utils/REST';
 export default function Table() {
+  const params=useParams();
+  const id=params.id;
   const [Asst,setAsst]=React.useState([]);
   var tab;
     async function getAsst()
     {
         try {
-            const resu = await FetchData("/assets",false,null);
+            const resu = await FetchData(`/${id}`,false,null);
             setAsst(resu.data.data);
         } catch (error) {
             
@@ -17,7 +20,16 @@ export default function Table() {
     React.useEffect(() => {
         getAsst();
     }, [])
-  
+    
+    if(id==="assets")
+    {
+        tab=Asst.map((asst)=>{
+            return <AssetCard key={asst._id} asst={asst} />
+        })
+    }
+    else if(id==="")
+    {
+      
     tab=Asst.map((asst)=>{
         return <AssetCard key={asst._id} asst={asst} />
     })
@@ -27,4 +39,4 @@ export default function Table() {
         {tab}
     </div>
   )
-}
+}}
